09f13048013204e5e88244d45f01f977c89f4ecb
Benedek Zsombor Ákos <zsomborbeni@gmail.com>
2020-05-29 22:32:49 +0200

    Update RestNameService.java

--- src/main/java/com/example/demo/service/RestNameService.java
+++ src/main/java/com/example/demo/service/RestNameService.java
@@ -14,10 +14,8 @@ import com.example.demo.model.NodeModel;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Service
 public class RestNameService {
     //
     HashMap<Integer, Integer> replicationDatabase = new HashMap<>();
