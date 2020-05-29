877967e761fedc7d8517d4dd8d184e814cecdb47
Benedek Zsombor Ákos <zsomborbeni@gmail.com>
2020-05-29 19:08:46 +0200

    Restful controller with HTTP methods

--- src/main/java/com/example/demo/controller/NameController.java
+++ src/main/java/com/example/demo/controller/NameController.java
@@ -1,89 +1,108 @@
 package com.example.demo.controller;
-import com.example.demo.service.MulticastListner;
+
+import com.example.demo.model.FileModel;
+import com.example.demo.model.NodeModel;
+import com.example.demo.service.MulticastService;
 import com.example.demo.service.RestNameService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.io.*;
-import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @RestController
 public class NameController {
+
     RestNameService nameService;
+
     public NameController() throws IOException {
-        nameService = new RestNameService();
-        ExecutorService threadPool = Executors.newFixedThreadPool(2);
-        threadPool.execute(new MulticastListner(nameService));
+        this.nameService = new RestNameService();
+        final ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        threadPool.execute(new MulticastService(this.nameService));
     }
 
+    @PostMapping("/AddNode")
+    public ResponseEntity<NodeModel> addNode(@RequestBody NodeModel node) {
+        try {
+            this.nameService.addNodeToMap(node.getName(), node.getIp());
+        } catch (IOException e) {
+            return new ResponseEntity<>(node, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        }
+        return new ResponseEntity<>(node, HttpStatus.CREATED);
+    }
 
-    @GetMapping("/AddNode")
-    public String output (@RequestParam(value = "Name", defaultValue = "omo") String name,@RequestParam(value = "Ip", defaultValue = "omo") String ip) throws IOException {
-        if (!name.equals("omo") && !ip.equals("omo")) {
-            nameService.addNodeToMap(name, ip);
-            return "node "+name+" with ip address "+ip+" was succesfully added to the node map";
+    @DeleteMapping("/RemoveNode")
+    public ResponseEntity<NodeModel> removeNode(@RequestBody NodeModel node)
+            throws NumberFormatException, IOException, InterruptedException {
+        try {
+            this.nameService.removeNodeFromMap(Integer.parseInt(node.getName()));
+        } catch (IOException e) {
+            return new ResponseEntity<>(node, HttpStatus.NOT_FOUND);
         }
-        else
-            return"adding new node failed";
+        return new ResponseEntity<>(node, HttpStatus.NO_CONTENT);
     }
-    @GetMapping("/RemoveNode")
-    public String output (@RequestParam(value = "ID", defaultValue = "omo") String ID) throws IOException, InterruptedException {
-        if (!ID.equals("omo")) {
 
-            nameService.removeNodeFromMap(Integer.parseInt(ID));
-            return "node "+ID+" was succesfully Removed from the node map";
-        }
-        else
-            return"removing node failed";
-    }
-    @GetMapping("/LocateFile")
-    public String output2 (@RequestParam(value = "fileName", defaultValue = "omo")String fileName,@RequestParam(value = "remove", defaultValue = "false") String remove) throws IOException, InterruptedException {
-        if (!fileName.equals("omo")){
-            if (nameService.requestFile(fileName) == -1)
-                return "File Not present in any of the nodes";
-            else
-            if(remove.equals("true")){
-                Integer temp = nameService.requestFile(fileName);
-                String ip = nameService.nodes.get(nameService.requestFile(fileName));
-                nameService.removeNodeFromMap(temp);
-                return "File "+fileName +" is located at node with ip: "+ip+" and the node was removed";
+    @GetMapping("/LocateFile/{fileName}")
+    public ResponseEntity<FileModel> locateFile(@PathVariable String fileName,
+            @RequestParam(value = "remove", defaultValue = "false") String remove) {
+        if (!fileName.isEmpty()) {
+            Integer nodeHash = nameService.requestFile(fileName);
+            String ip = nameService.nodes.get(nodeHash);
+            if (nodeHash == -1)
+                return new ResponseEntity<>(new FileModel(fileName), HttpStatus.NOT_FOUND);
+            else if (remove.equals("true")) {
+                try {
+                    this.nameService.removeNodeFromMap(nodeHash);
+                } catch (IOException | InterruptedException e) {
+                    return new ResponseEntity<>(new FileModel(ip, fileName), HttpStatus.NOT_FOUND);
+                }
+                return new ResponseEntity<>(new FileModel(fileName), HttpStatus.NO_CONTENT);
             }
-            else
-                return "File "+fileName +" is located at node with ip: "+nameService.nodes.get(nameService.requestFile(fileName));
+            return new ResponseEntity<>(new FileModel(ip, fileName), HttpStatus.OK);
         }
-        return "This command requires a filename";
+        return new ResponseEntity<>(new FileModel(fileName), HttpStatus.BAD_REQUEST);
     }
-    @GetMapping("/AddFile")
-    public String addFile (@RequestParam(value = "Name", defaultValue = "omo") String name,@RequestParam(value = "File", defaultValue = "omo") String file) throws IOException, InterruptedException {
-        if (!name.equals("omo") && !file.equals("omo")) {
-            if (nameService.addFileToDataBase(name,file)==1) {
-                System.out.println("Running /AddFile, name " + name + " file  " + file);
-                return "file " + file + " located at " + name + " was succesfully added to the node map";
+
+    @PostMapping("/AddFile")
+    public ResponseEntity<FileModel> addFile(@RequestBody FileModel file) {
+        if (!file.getNode().isEmpty() && !file.getFile().isEmpty()) {
+            try {
+                if (nameService.addFileToDataBase(file.getNode(), file.getFile()) == 1) {
+                    System.out.println("Running /AddFile, name " + file.getNode() + " file  " + file.getFile());
+                    return new ResponseEntity<>(file, HttpStatus.CREATED);
+                } else
+                    return new ResponseEntity<>(file, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+            } catch (IOException | InterruptedException e) {
+                return new ResponseEntity<>(file, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
             }
-            else
-                return "file "+file+" located at "+name+" was not added to the node map, Unexisting node";
         }
-        else
-            return"removing node failed";
+        return new ResponseEntity<>(file, HttpStatus.BAD_REQUEST);
     }
-    @GetMapping("/RemoveFile")
-    public String removeFile (@RequestParam(value = "Name", defaultValue = "omo") String name,@RequestParam(value = "File", defaultValue = "omo") String file) throws IOException, InterruptedException {
-        if (!name.equals("omo") && !file.equals("omo")) {
-            if (nameService.removeFileFromDatabase(name,file)==1) {
-                //System.out.println("Ik run nu /AddFile, Variebelen name " + name + " file  " + file);
-                return "file " + file + " located at " + name + " was succesfully removed";
+
+    @DeleteMapping("/RemoveFile")
+    public ResponseEntity<FileModel> removeFile(@RequestBody FileModel file) {
+        if (!file.getNode().isEmpty() && !file.getFile().isEmpty()) {
+            try {
+                if (nameService.removeFileFromDatabase(file.getNode(), file.getFile()) == 1) {
+                    return new ResponseEntity<>(file, HttpStatus.NO_CONTENT);
+                } else
+                    return new ResponseEntity<>(file, HttpStatus.NOT_FOUND);
+            } catch (IOException | InterruptedException e) {
+                return new ResponseEntity<>(file, HttpStatus.NOT_FOUND);
             }
-            else
-                return "file "+file+" located at "+name+" was not removed to the node map, Unexisting node";
         }
-        else
-            return"removing node failed";
+        return new ResponseEntity<>(file, HttpStatus.BAD_REQUEST);
     }
+
     @GetMapping("/Database")
-    public String database(){
-        return nameService.dataBase.toString();
+    public ResponseEntity<String> database() {
+        return new ResponseEntity<String>(nameService.dataBase.toString(), HttpStatus.OK);
     }
 }
